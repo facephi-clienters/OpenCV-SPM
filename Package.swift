958466c35653ee05e81a8a296d5679b91d2318b6
Jorge Poveda <jpoveda@facephi.com>
2025-02-05 11:13:58 +0100

    fix: Added supported platforms

--- Package.swift
+++ Package.swift
@@ -8,19 +8,15 @@ let checksum = "8fb2dc19a9030b70219fbbe55f52b86f19d9ba9bbc6ae84d17d6062d30293ab8
 
 let package = Package(
     name: "OpenCV-SPM",
+    platforms: [
+        .macOS(.v10_13), .iOS(.v12), .macCatalyst(.v13)
+    ],
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "OpenCV-SPM",
-            targets: ["OpenCV-SPM", "opencv2"]),
+            targets: ["opencv2"]),
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
-        .target(
-            name: "OpenCV-SPM",
-            dependencies: ["opencv2"]
-        ),
         .binaryTarget(name: "opencv2",
                       url: "https://github.com/facephi-clienters/OpenCV-SPM/releases/download/\(version)/opencv2.xcframework.zip",
                       checksum: checksum),
